540761c808061eddee1472795de4f3004a65d028
stolinski <scotttolinski@gmail.com>
2012-11-14 19:45:32 -0500

    Cleans up Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Resolution::Application.routes.draw do
-  get "static_pages/home"
+  root :to => 'static_pages#home'
   match '/about', :to => "static_pages#about"
-  get "static_pages/help"
-  get "static_pages/contact"
+  match '/contact', :to => "static_pages#contact"
+  match '/help', :to => "static_pages#help"
+
 
   resources :posts
 
@@ -56,7 +57,6 @@ Resolution::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,33 +4,36 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Monthly Resolutions'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_content('Monthly Resolutions')
+      # visit root_path
+      # page.should have_content('Monthly Resolutions')
     end
   end
+
   describe 'Help page' do
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_content('Help')
   	end
   end
+
   describe 'About Page' do
   	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_content('About Us')
   	end
   end
+
   describe "Contact page" do
 
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title',
-                    :text => "Monthly Resolutions | Contact")
+      visit contact_path
+      page.should have_selector('title', :text => "Monthly Resolutions | Contact")
     end
   end
+
 end
