265528d6dd434ad0307f38d66c8629dd30642e6d
stolinski <scotttolinski@gmail.com>
2012-11-15 17:56:17 -0500

    Sign Up Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Resolution::Application.routes.draw do
   root :to => 'static_pages#home'
+  resources :users
+  
+  match '/signup', :to => "users#new"
+
   match '/about', :to => "static_pages#about"
   match '/contact', :to => "static_pages#contact"
   match '/help', :to => "static_pages#help"
@@ -7,7 +11,7 @@ Resolution::Application.routes.draw do
 
   resources :posts
 
-  resources :users
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,39 +1,29 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Monthly Resolutions'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      # visit root_path
-      # page.should have_content('Monthly Resolutions')
-    end
+    before { visit root_path }
+
+    it { should have_selector( 'title', :text => full_title('')) }
   end
 
   describe 'Help page' do
-  	it "should have the content 'Help'" do
-  		visit help_path
-  		page.should have_content('Help')
-  	end
+    before { visit help_path }
+
+  	it { should have_content('Help') }
   end
 
   describe 'About Page' do
-  	it "should have the content 'About Us'" do
-  		visit about_path
-  		page.should have_content('About Us')
-  	end
+    before { visit about_path }
+  	it { should have_content('About Us') }
   end
 
   describe "Contact page" do
-
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', :text => 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title', :text => "Monthly Resolutions | Contact")
-    end
+    before { visit contact_path }
+    it { should have_selector('h1', :text => 'Contact') }
+    it { should have_selector('title', :text => full_title('Contact')) }
   end
 
 end
